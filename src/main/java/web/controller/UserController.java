d658d34cd559a020bb94c4293ccd214bf82cf432
osian <oisa.nikifor@gmail.com>
2024-08-08 17:20:34 +0300

    Initial commit

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,71 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.User;
+import web.service.UserService;
+import web.service.UserServiceImpl;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public String getAllUsers(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "all_users";
+    }
+
+    @GetMapping("/new")
+    public String showFormAddUser(Model model) {
+        model.addAttribute("user", new User());
+        return "add_user";
+    }
+
+    @PostMapping()
+    public String addUser(@ModelAttribute("user") User user) {
+        userService.saveUser(user.getFirstName(), user.getLastName(), user.getEmail());
+        return "redirect:/add_user";
+    }
+
+    @GetMapping("/show")
+    public String showUserById(@RequestParam("id") int id, Model model) {
+        User user = userService.showUserById(id);
+        if (user == null) {
+            return "redirect:/all_user";
+        } else {
+            model.addAttribute("user", user);
+            return "selected_user";
+        }
+    }
+
+    @GetMapping("/show/edit")
+    public String editUser(Model model, @RequestParam("id") int id) {
+        model.addAttribute("user", userService.showUserById(id));
+        return "edit_user";
+    }
+
+    @PostMapping("/update")
+    public String updateUser(@ModelAttribute("user") User user, @RequestParam("id") int id) {
+        userService.updateUserById(id, user);
+        return "redirect:/all_user";
+    }
+
+    @PostMapping("/show/delete")
+    public String deleteUser(@ModelAttribute("user") User user, @RequestParam("id") int id) {
+        userService.removeUserById(id);
+        return "redirect:/all_user";
+    }
+}
